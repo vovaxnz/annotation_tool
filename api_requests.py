29d8f4f58da87be5d13b0de494dfe74148410c22
vovaxnz <vova.xnz@gmail.com>
2024-05-24 11:51:16 +0300

    Add retry 2 times when project opening

--- api_requests.py
+++ api_requests.py
@@ -10,24 +10,27 @@ def get_projects_data() -> List[ProjectData]:
     url = f'{settings.api_url}/api/annotation/projects_data/'
 
     data = {'user_token': settings.token}
-    response = requests.post(url, json=data)
 
-    if response.status_code != 200:
-        raise MessageBoxException(f"Unable to get projects data. {response.status_code}")
+    for i in range(2):
+        response = requests.post(url, json=data)
+
+        if response.status_code == 200:
+        
+            projects = response.json()["projects"]
+
+            result = list()
+            for project in projects:
+                result.append(
+                    ProjectData(
+                        id=project["id"],
+                        uid=project["uid"],
+                        stage=getattr(AnnotationStage, project["annotation_stage"]),
+                        mode=getattr(AnnotationMode, project["annotation_mode"])
+                    )
+                )
+            return result
     
-    projects = response.json()["projects"]
-
-    result = list()
-    for project in projects:
-        result.append(
-            ProjectData(
-                id=project["id"],
-                uid=project["uid"],
-                stage=getattr(AnnotationStage, project["annotation_stage"]),
-                mode=getattr(AnnotationMode, project["annotation_mode"])
-            )
-        )
-    return result
+    raise MessageBoxException(f"Unable to get projects data. {response.status_code}")
 
 
 def get_project_data(project_uid: str) -> Tuple[AnnotationStage, AnnotationMode, str]:
